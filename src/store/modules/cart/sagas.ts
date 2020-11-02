e1d64432a9ece8a06b7eb5e9e53fbb5754c20bce
Matheus Gonçalves <matheusglmonteiro@gmail.com>
2020-11-02 17:21:32 -0300

    Aplication redux

---
+++ src/store/modules/cart/sagas.ts
@@ -0,0 +1,19 @@
+import { all, takeLatest, select } from 'redux-saga/effects';
+import { IState } from '../..';
+import { addProductToCart } from './actions';
+
+type checkProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: checkProductStockRequest) {
+  const { product } = payload; 
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  });
+
+  console.log(currentQuantity);
+}
+
+export default all([
+  takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)
+]);
